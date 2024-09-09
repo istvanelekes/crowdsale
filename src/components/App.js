95cd914d0ab39a15542bca09f7d029ba3ef5a450
Istvan Elekes <istvan.elekes@partner.rolls-roycemotorcars.com>
2024-09-09 21:26:00 +0300

    Load Accounts

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
-import Container from 'react-bootstrap/Container';
+import { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { ethers } from 'ethers';
+
+// Components
 import Navigation from './Navigation';
+import Info from './Info';
 
 function App() {
-  return (
-    <Container>
-        <Navigation />
-    </Container>
-  )
+
+    const [account, setAccount] = useState(null)
+
+    const loadBlockchainData = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        console.log(provider)
+
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'})
+        const account = ethers.utils.getAddress(accounts[0])
+        setAccount(account)
+    }
+
+    useEffect(() => {
+        loadBlockchainData();
+    })
+
+    return (
+        <Container>
+            <Navigation />
+            <hr/>
+            {account && (
+                <Info account = {account} />
+            )}
+        </Container>
+    )
 }
 
 export default App;
